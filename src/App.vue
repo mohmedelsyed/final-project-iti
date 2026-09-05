2c5e6c9a2eb465355cf9874d7c66bfdf0ff5454a
HELAL <helal@iti.edu.eg>
2026-09-05 11:26:07 +0300

    fix: remove unused FeaturedCars component import and references

--- src/App.vue
+++ src/App.vue
@@ -9,9 +9,6 @@
     <!-- Buyer / Seller Selection Component -->
     <RoleSelector @role-change="handleRoleChange" />
 
-    <!-- Featured Cars Component -->
-    <FeaturedCars ref="carsRef" />
-
     <!-- Services Component -->
     <ServicesSection />
 
@@ -25,18 +22,11 @@ import { ref } from 'vue'
 import Navbar from './components/Navbar.vue'
 import HeroSection from './components/HeroSection.vue'
 import RoleSelector from './components/RoleSelector.vue'
-import FeaturedCars from './components/FeaturedCars.vue'
 import ServicesSection from './components/ServicesSection.vue'
 import FooterSection from './components/FooterSection.vue'
 
-const carsRef = ref(null)
-
 const handleHeroSearch = (filters) => {
   console.log('Search triggered with filters:', filters)
-  const carsEl = document.getElementById('featured-cars')
-  if (carsEl) {
-    carsEl.scrollIntoView({ behavior: 'smooth' })
-  }
 }
 
 const handleRoleChange = (role) => {
